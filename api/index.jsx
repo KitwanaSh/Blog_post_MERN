c2e7e9550df9211b82e74f7c81baf6f434210f66
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-06-29 11:52:14 +0200

    Feat: Start the api

--- api/index.jsx
+++ api/index.jsx
@@ -1,8 +1,23 @@
 const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/User')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json("test ok")
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://blog:G1bIkIhkdMZFlMVL@cluster0.hnphmkw.mongodb.net/")
+
+app.post('/register', async (req, res) => {
+    const {username,password} = req.body;
+    try{
+        const userDoc = await User.create({username, password});
+        res.json(userDoc)
+    } catch(e) {
+        res.status(400).json(e)
+    }
 })
 
 app.listen(4000)
+ 
